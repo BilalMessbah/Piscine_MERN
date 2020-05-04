0f98a6d1873f5a698f329033d5346d07df39ff5d
Bilal Messbah <bilal.messbah@epitech.eu>
2020-05-04 15:09:55 +0200

    exo 4

--- Piscine_MERN_Jour_01/ex_04/server.js
+++ Piscine_MERN_Jour_01/ex_04/server.js
@@ -1,5 +1,6 @@
-const express = require('express')
-const app = express()
+var express = require('express')
+var app = express();
+var router = express.Router();
 
 process.env.NODE_ENV = 'development';
 
@@ -7,48 +8,37 @@ var http = require('http');
 var fs = require('fs');
 nconf = require('nconf');
 
-
-//
-// Setup nconf to use (in-order):
-//   1. Command-line arguments
-//   2. Environment variables
-//   3. A file located at 'path/to/config.json'
-//
 nconf.argv()
- .env()
- .file({ file: 'path/to/config.json' });
+  .env()
+  .file({ file: 'path/to/config.json' });
 
-//
-// Set a few variables on `nconf`.
-//
 nconf.set('database:host', 'localhost');
 nconf.set('database:port', 4242);
 
-//
-// Get the entire database object from nconf. This will output
-// { host: '', port:  }
-//
-console.log('foo: ' + nconf.get('foo'));
 console.log('NODE_ENV: ' + nconf.get('NODE_ENV'));
 console.log('database: ' + nconf.get('database'));
 
-//
-// Save the configuration object to disk
-//
+ app.get('/', function (req, res, next) {
+   res.send('Bonjour, veuillez mettre un /name/:name');
+ })
 
-//console.log('NODE_ENV');
-http.createServer(function (req, res) {
-  fs.readFile('index.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
+app.get('/name', function (req, res, next) {
+  fs.readFile('index.html', function (err, data) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write(data);
+    res.write('unknown');
     return res.end();
   });
-}).listen(4242);
-
-app.get('/', function (req, res) {
-  res.send('Voila ca a l\'air de bien marcher dis donc')
 })
 
-// app.listen(4242, function () {
-//   console.log('Le serveur se lance sur le port 4242 !')
-// })
+app.get('/name/:nom', function (req, res, next) {
+  console.log(req.params.nom);
+  var name = req.params.nom;
+  fs.readFile('index.html', function (err, data) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(data);
+    res.write(name);
+    return res.end();
+  });
+});
+app.listen(4242);
