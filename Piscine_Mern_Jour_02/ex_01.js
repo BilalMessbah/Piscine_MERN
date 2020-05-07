32d2c998c8aedde0b83075a98e5ec1ec3adc814b
Bilal Messbah <bilal.messbah@epitech.eu>
2020-05-07 20:01:15 +0200

    ex02 sans react

--- Piscine_Mern_Jour_02/ex_01.js
+++
@@ -1,1 +0,0 @@
-"C:\Program Files\MongoDB\Server\4.2\bin\mongo.exe" --port 27042
--- Piscine_Mern_Jour_02/ex_04/ex_04.js
+++ Piscine_Mern_Jour_02/ex_04/ex_04.js
@@ -10,3 +10,49 @@ MongoClient.connect(url, function(err, db) {
             console.log("Connection successfull");
         }
 });
+
+
+const express = require('express')
+const app = express()
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://127.0.0.1:27042';
+const dbName = 'api';
+const assert = require('assert');
+var bodyParser = require('body-parser');
+
+MongoClient.connect(url, function(err, client) {
+    assert.equal(null, err);
+    console.log("Connected correctly to server");
+  
+    const db = client.db(dbName);
+    app.use(bodyParser.urlencoded({extended: false}));
+    app.post('/register', function(req, res){
+        //console.log(req.body.user.login);
+        var login = req.body.login;
+        var email = req.body.email;
+        var password = req.body.password;
+        db.collection('membre').insertMany([req.body], function(err, r) {
+            //delete req.body._id;
+          assert.equal(null, err);
+          assert.equal(2, r.insertedCount);
+    
+          //client.close();
+        });
+        res.send('Collection saved.\n' + JSON.stringify(req.body));
+    })
+});
+
+app.set('view engine', 'ejs');
+
+app.get("/login", (req, res) => {
+    res.render("login");
+})
+
+app.get("/", (req, res) => {
+    res.render("register");
+})
+
+app.listen("4242");
+
+
+
